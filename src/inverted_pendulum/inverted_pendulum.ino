e99c2291da34ac7809c9d40506d2a8122a52553f
tloinny <tloinny@gmail.com>
2019-08-28 10:47:31 +0800

    use a new motor driver 8/28/2019 10:47AM

--- src/inverted_pendulum/inverted_pendulum.ino
+++ src/inverted_pendulum/inverted_pendulum.ino
@@ -1,41 +1,30 @@
-#include <L298N.h>
+#include <MotorDriver.h>
+#include <seeed_pwm.h>
 #include <AS5600.h>
 #include <easy_inverted_pendulum.h>
 #include <Wire.h>
 #include <Ultrasonic.h>
 #include <Math.h>
 
-#define SWING_UP_RANG 120
+#define SWING_UP_RANG 170
 #define WITHIN_PID_SCOPE (Angle_encoder > Inverted_pendulum_controller.GetAngleSetPoint() - SWING_UP_RANG / 2 && Angle_encoder < Inverted_pendulum_controller.GetAngleSetPoint() + SWING_UP_RANG / 2)
 #define WITHIN_SWING_SCOPE (Angle_encoder <= Inverted_pendulum_controller.GetAngleSetPoint() - SWING_UP_RANG / 2 || Angle_encoder >= Inverted_pendulum_controller.GetAngleSetPoint() + SWING_UP_RANG / 2)
-#define offset 10
+#define offset 0
 
-const int EN = 4;
-const int BRK = 5;
-const int DIR = 6;
-const int PWM = 9;
+#define A_kp 46
+#define A_kd 4000
 
-#define A_kp 77.5 * 0.6
-#define A_kd 8500 * 0.6
-
-#define P_kp 1.3
-#define P_kd 2200*0.6
+#define P_kp 1
+#define P_kd 840
 
 const long A_sample_time = 0;
 const long P_sample_time = 0;
 const int A_limit = 255;
 const int P_limit = 255;
 
-const int ENA = 5;
-const int IN1 = 6;
-const int IN2 = 7;
-const int IN3 = 9;
-const int IN4 = 10;
-const int ENB = 11;  /* only one motor */
+MotorDriver motor;
 
-L298N DCMotor(ENA,IN1,IN2,IN3,IN4,ENB); /* motor driver */
-
-Ultrasonic ultrasonic(8);
+Ultrasonic ultrasonic(4);
 
 AMS_5600 ams_5600;	/* angle encoder */
 inverted_pendulum Inverted_pendulum_controller(A_kp, A_kd, P_kp, P_kd, A_sample_time, P_sample_time, A_limit, P_limit);	/* inverted pendulum pid controller 35, 90, 5.5, 50, 200, 100 */
@@ -95,6 +84,8 @@ void setup()
 	Inverted_pendulum_controller.SetAngleSetPoint(178.31);
 	Inverted_pendulum_controller.SetPositionSetPoint(20.5*10);
 
+	motor.begin();
+
 	Serial.println("Invered Pendulum");
 	Serial.print("A_KP:");
 	Serial.print(A_kp);
@@ -122,35 +113,27 @@ void loop()
 	Angle_encoder = convertScaledAngleToDegrees(ams_5600.getScaledAngle());
 	ultrasonic.MeasureInCentimeters();
 	Position_encoder = round(ultrasonic.RangeInCentimeters*10);
-	if (abs(Position_encoder - Inverted_pendulum_controller.GetPositionSetPoint()) <= 0)
-	{
-		Position_encoder = Inverted_pendulum_controller.GetPositionSetPoint();
-	}
 
-	if (abs(Angle_encoder - Inverted_pendulum_controller.GetAngleSetPoint()) <= 0)
-	{
-		Angle_encoder = Inverted_pendulum_controller.GetAngleSetPoint();
-	}
-	if (WITHIN_PID_SCOPE && Position_encoder >= 40 && Position_encoder <= 400)
+	if (WITHIN_PID_SCOPE && Position_encoder >= 30 && Position_encoder <= 400)
 	{
 		output = Inverted_pendulum_controller.InvertedPendulumUpdate(Angle_encoder, Position_encoder);
 		if (output > 0)
 		{
 			throttle = output + offset;
 				if(throttle > 255) throttle = 255;
-			DCMotor.backward(throttle,0);
+			motor.speed(0, throttle);
 		}else if (output < 0)
 		{
 			output = -1*output;
 			throttle = output + offset;
 				if(throttle > 255) throttle = 255;
-			DCMotor.forward(throttle,0);
+			motor.speed(0, -1*throttle);
 		}else
 		{
-			DCMotor.full_stop(0);
+			motor.brake(0);
 		}
 	}else
 	{
-		DCMotor.full_stop(0);
+		motor.brake(0);
 	}
 }
